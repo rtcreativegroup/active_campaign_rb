61b532beae3f2c656a5c94501bfdc47e1381e31d
Brent Odell <brentdodell@gmail.com>
2018-07-03 15:40:00 -0500

    Add Connection spec

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe ActiveCampaign::Connection do
-  describe '#initialize' do
-    subject(:connection) do
-      described_class.new(
-        base_url: 'http://www.example.com',
-        key: 'abc123',
-        adapter: adapter
-      )
-    end
-    let(:adapter) { nil }
-    let(:versioned_base_url) { 'http://www.example.com' }
+  subject(:connection) do
+    described_class.new(
+      base_url: base_url,
+      key: 'abc123',
+      adapter: adapter
+    )
+  end
+  let(:base_url) { 'http://www.example.com' }
+  let(:adapter) { nil }
+  let(:versioned_base_url) { 'http://www.example.com' }
 
+  describe '#initialize' do
     shared_examples 'takes a base_url and an optional adapter' do
       it 'calls adapter with base_url and sets return value to attr' do
         expect(connection.base_url.to_s).to eq(versioned_base_url)
@@ -52,6 +53,125 @@ RSpec.describe ActiveCampaign::Connection do
       it "defaults key to ENV['ACTIVE_CAMPAIGN_KEY']" do
         expect(connection.key).to eq('my-environmental-key')
       end
+    end
+  end
+
+  context 'HTTP verbs' do
+    shared_examples_for 'request method' do |method|
+      it "calls #{method.to_s.upcase} on the supplied uri appended to the base_uri" do
+        shared_stub_request(
+          method: method,
+          request: {
+            query: {
+              api_key: 'abc123',
+              api_output: 'json',
+              test_key: 'test_value',
+            }
+          },
+          response: {
+            body: {
+              response: 'test_response'
+            }.to_json,
+            status: 200
+          }
+        )
+
+        expect(connection.public_send(method, '/test', params: { test_key: 'test_value' }))
+          .to eq('response' => 'test_response')
+      end
+
+      context 'with header args' do
+        it 'sets the header args' do
+          shared_stub_request(
+            method: method,
+            request: {
+              query: {
+                api_key: 'abc123',
+                api_output: 'json',
+              },
+              headers: {
+                'Content-Type': 'application/json'
+              },
+            }
+          )
+
+          connection.public_send(method, '/test', headers: { 'Content-Type': 'application/json' })
+        end
+      end
+
+      context 'with form args' do
+        it 'sets the Content-Type to application/x-www-form-urlencoded' do
+          shared_stub_request(
+            method: method,
+            request: {
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+              },
+              query: {
+                api_key: 'abc123',
+                api_output: 'json',
+              },
+            }
+          )
+
+          connection.public_send(method, '/test', form: { test_key: 'test_value' })
+        end
+      end
+
+      context 'error cases' do
+        [400, 403, 404, 500].each do |code|
+          it "returns the parsed error for status code #{code}" do
+            shared_stub_request(
+              method: method,
+              request: {
+                query: {
+                  api_key: 'abc123',
+                  api_output: 'json',
+                },
+              },
+              response: {
+                status: code,
+                body: {
+                  status: 'error',
+                  error: 'message'
+                }.to_json
+              }
+            )
+
+            expect(connection.public_send(method, '/test')).to(
+              eq('status' => 'error', 'error' => 'message'),
+              "status code #{code} did not return proper value"
+            )
+          end
+        end
+      end
+
+      def shared_stub_request(
+        method:,
+        path: '/test',
+        request: { body: '' },
+        response: { body: {}.to_json }
+      )
+        stub_request(method, base_url + path)
+          .with(request)
+          .to_return(response)
+      end
+    end
+
+    describe '#get' do
+      it_behaves_like 'request method', :get
+    end
+
+    describe '#post' do
+      it_behaves_like 'request method', :post
+    end
+
+    describe '#put' do
+      it_behaves_like 'request method', :put
+    end
+
+    describe '#delete' do
+      it_behaves_like 'request method', :delete
     end
   end
 end
