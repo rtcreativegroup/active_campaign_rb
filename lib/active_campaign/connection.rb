53be8c161cbc466f4241bc65031fb55b080f2d4c
Brent Odell <brentdodell@gmail.com>
2018-07-03 13:24:12 -0500

    Add HTTP verbs to Connection

--- lib/active_campaign/connection.rb
+++ lib/active_campaign/connection.rb
@@ -13,5 +13,43 @@ module ActiveCampaign
       @base_url = adapter.(base_url)
       @key = key
     end
+
+    def get(endpoint, args={})
+      request(:get, endpoint: endpoint, args: args)
+    end
+
+    def post(endpoint, args={})
+      request(:post, endpoint: endpoint, args: args)
+    end
+
+    def put(endpoint, args={})
+      request(:put, endpoint: endpoint, args: args)
+    end
+
+    def delete(endpoint, args={})
+      request(:delete, endpoint: endpoint, args: args)
+    end
+
+    private
+
+    def request(action, endpoint:, args: {})
+      uri = URI(base_url + endpoint)
+      params_with_defaults = args.fetch(:params, {}).merge(default_params)
+
+      response = HTTP.send(action, uri, args.merge(params: params_with_defaults))
+
+      parse_response(response)
+    end
+
+    def default_params
+      {
+        api_key: key,
+        api_output: :json
+      }
+    end
+
+    def parse_response(response)
+      JSON.parse(response.body)
+    end
   end
 end
