d4ce71fdbab5318ab26aadb46d3a252608a39551
Brent Odell <brentdodell@gmail.com>
2018-07-10 11:50:36 -0500

    Add Tracking client

--- lib/active_campaign.rb
+++ lib/active_campaign.rb
@@ -5,6 +5,7 @@ require 'active_campaign/v2/adapters/admin_adapter'
 require 'active_campaign/v2/adapters/event_adding_adapter'
 require 'active_campaign/v2/adapters/event_management_adapter'
 require 'active_campaign/v2/adapters/site_tracking_adapter'
+require 'active_campaign/v2/adapters/site_tracking_code_adapter'
 require 'active_campaign/v3/adapter'
 
 module ActiveCampaign
--- lib/active_campaign/v2/clients/tracking.rb
+++ lib/active_campaign/v2/clients/tracking.rb
@@ -2,6 +2,38 @@ module ActiveCampaign
   module V2
     module Clients
       module Tracking
+        def track_event_delete(event:)
+          delete(:event_management, form: { event: event })
+        end
+
+        def track_event_list
+          get(:event_management)
+        end
+
+        def track_event_status_edit(status:)
+          post(:event_management, form: { status: status })
+        end
+
+        def track_site_code
+          get(:site_tracking_code)
+        end
+
+        def track_site_list
+          get(:site_tracking)
+        end
+
+        def track_site_status_edit(status:)
+          post(:site_tracking, form: { status: status })
+        end
+
+        def track_site_whitelist_add(domain:)
+          put(:site_tracking, form: { domain: domain })
+        end
+
+        def track_site_whitelist_delete(domain:)
+          delete(:site_tracking, form: { domain: domain })
+        end
+
         def track_event_add(event:, **args)
           post(
             :event_adding,
