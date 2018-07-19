f903905ceb4f728ea21f1ecd09061868cc611cb0
Brent Odell <brentdodell@gmail.com>
2018-07-19 13:00:36 -0500

    Make V2::Client work more along the lines of V3
    - Also removes V2 Client spec
      - After this refactoring it works nothing like it did when this was
      written. We probably need to write specs for this and for V3 Client,
      as well as all the object-specific clients (Contact, Form, List, etc)

--- lib/active_campaign/settings.rb
+++ lib/active_campaign/settings.rb
@@ -6,5 +6,7 @@ module ActiveCampaign
 
     setting :base_url, ENV['ACTIVE_CAMPAIGN_URL']
     setting :api_key, ENV['ACTIVE_CAMPAIGN_KEY']
+    setting :tracking_account_id, ENV['ACTIVE_CAMPAIGN_TRACKING_ACCOUNT_ID']
+    setting :event_key, ENV['ACTIVE_CAMPAIGN_EVENT_KEY']
   end
 end
--- lib/active_campaign/v2/clients/tracking.rb
+++ lib/active_campaign/v2/clients/tracking.rb
@@ -3,40 +3,41 @@ module ActiveCampaign
     module Clients
       module Tracking
         def track_event_delete(event:)
-          delete(:event_management, body: { event: event })
+          delete('/api/2/track/event', body: { event: event })
         end
 
         def track_event_list
-          get(:event_management)
+          get('/api/2/track/event')
         end
 
         def track_event_status_edit(status:)
-          post(:event_management, body: { status: status })
+          post('/api/2/track/event', body: { status: status })
         end
 
         def track_site_code
-          get(:site_tracking_code)
+          get('/api/2/track/site/code')
         end
 
         def track_site_list
-          get(:site_tracking)
+          get('/api/2/track/site')
         end
 
         def track_site_status_edit(status:)
-          post(:site_tracking, body: { status: status })
+          post('/api/2/track/site', body: { status: status })
         end
 
         def track_site_whitelist_add(domain:)
-          put(:site_tracking, body: { domain: domain })
+          put('/api/2/track/site', body: { domain: domain })
         end
 
         def track_site_whitelist_delete(domain:)
-          delete(:site_tracking, body: { domain: domain })
+          delete('/api/2/track/site', body: { domain: domain })
         end
 
         def track_event_add(event:, **args)
           post(
-            :event_adding,
+            '',
+            base_uri: 'https://trackcmp.net/event',
             query: { api_action: 'track_event_add' },
             body: args.merge(event: event)
           )
